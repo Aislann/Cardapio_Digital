8e3acb82fb3a570d665de86e7e9d14532d72a147
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2024-01-25 23:35:05 -0300

    Add files via upload

---
+++ src/Cardapio.jsx
@@ -0,0 +1,479 @@
+import React from 'react';
+import './Cardapio.css'
+import image from '../public/logoQuentinha.png'
+
+const Cardapio = () => {
+  return (
+    <div className='container'>
+      <img src={image} alt="" />
+      {/* <span>Quentinhas</span>
+      <span>Do Grau</span> */}
+      <h2 className=''>ALMOÇO</h2>
+      <div className="menu">
+      
+        <div className="cardapio-card">
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2 className=''>JANTAR</h2>
+      <div className="menu">
+      
+        <div className="cardapio-card">
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2 className=''>BEBIDAS</h2>
+      <div className="menu">
+      
+        <div className="cardapio-card">
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2 className=''>SOBREMESAS</h2>
+      <div className="menu">
+      
+        <div className="cardapio-card">
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="wrapperGrid">
+            <div className="wrapper-thumbnail">
+              <figure className="thumbnail" style={{ backgroundColor: 'rgb(242, 229, 212)' }}>
+                <img src="https://d3sn2rlrwxy0ce.cloudfront.net/chicken-duplo-thumb.png?mtime=20210916143308&amp;focal=none" alt="Chicken Duplo" title="Chicken Duplo" />
+              </figure>
+            </div>
+
+            <div className="coupon-info">
+              <div className="inner">
+                <div className="desc">
+                  <h3>Chicken Duplo</h3>
+                  <h3>R$ 99,99</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+
+
+
+
+    </div>
+
+
+
+  );
+};
+
+export default Cardapio;
